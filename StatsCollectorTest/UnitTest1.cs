148808963101b50c0f13b65ae9bdbf1bca251167
Bharath d <Bharath d>
2016-03-07 15:10:03 +0530

    .net Project

---
+++ StatsCollectorTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StatCollector;
+
+namespace StatsCollectorTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("HELLO", "HELLO");
+        }
+    }
+}
