09163e13d0302426faeb9e78af611e163641923d
Bharath <bharath82.d@gmail.com>
2016-06-12 22:18:31 +0530

    Mater commit on sUNDAY
    # Conflicts:
    #	StatsCollector/StatsCollector.feature.cs

--- StatsCollector/StatsCollector.feature.cs
+++ StatsCollector/StatsCollector.feature.cs
@@ -71,6 +71,9 @@ namespace StatsCollector
         {
             TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Calculate The StatCollector Indicators for Nacre Source:", new string[] {
                         "mytag"});
+#line 14
+#line 13
+#line 12
 #line 12
 this.ScenarioSetup(scenarioInfo);
 #line hidden
@@ -89,9 +92,9 @@ this.ScenarioSetup(scenarioInfo);
                         "Kate",
                         "21",
                         "1"});
-#line 13
+#line 15
  testRunner.Given("I have the Following Nacre Row", ((string)(null)), table1, "Given ");
-#line 17
+#line 19
  testRunner.When("I Invoke the Stats Collector", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
 #line hidden
             TechTalk.SpecFlow.Table table2 = new TechTalk.SpecFlow.Table(new string[] {
@@ -106,7 +109,7 @@ this.ScenarioSetup(scenarioInfo);
             table2.AddRow(new string[] {
                         "COUNT_NULL_Name",
                         "0"});
-#line 18
+#line 20
  testRunner.Then("the result should be as Below", ((string)(null)), table2, "Then ");
 #line hidden
             this.ScenarioCleanup();
